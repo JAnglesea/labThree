0d5d2bfa68da4fdfb9d5c2228784e1c4dc284ee4
JAnglesea <43370631+JAnglesea@users.noreply.github.com>
2019-02-07 10:26:52 +0000

    Add files via upload

---
+++ src/labthree/LabThree.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labthree;
+
+/**
+ *
+ * @author 1810595
+ */
+public class LabThree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //2
+        
+        do {
+        int diceOne = (int)(Math.random()*6) + 1;
+        int diceTwo = (int)(Math.random()*6) + 1;
+        
+        } while ( bool rollAgain = true);
+       
+        
+        
+    }
+    
+}
